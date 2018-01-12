a0e1dc3d3919b6c89bff4be1467031acc75abdde
oALeixo <33841066+oALeixo@users.noreply.github.com>
2018-01-12 18:08:59 -0200

    Update pools list

--- config.js
+++ config.js
@@ -7,6 +7,12 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "nbr": [
-		["nbr.4miner.start", "http://api-cryptonote.4miner.me:8118/stats"],
+  ["nbr.openminer.net", "http://nbr.openminer.net:8117"],
+  ["nbr.selvahost.com.br", "http://nb.selvahost.com.br:8999"],
+        ["nbr.miner10.in", "http://nbr.miner10.in:8117"],
+        ["nbr.4miner.me", "http://us-nb.4miner.me:8118"],
+        ["niobiopool.com.br", "http://45.79.150.254:8119"],
+  ["nbr.datawebinsider.com.br", "http://nbr.datawebinsider.com.br:8117"],
+  ["stminerpool.com.br", "stminerpool.com.br:8117"]
     ]
 };
